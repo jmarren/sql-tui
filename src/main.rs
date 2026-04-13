35b69a5c6ee4e5201cbbcc984d37e7ebd2550ec0
jmarren <jmarren99@gmail.com>
2026-04-12 17:21:21 -0700

    displaying text and int4 in table

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,67 @@
 use ratatui_textarea::TextArea;
 use crossterm::event::{Event, KeyCode, KeyModifiers, ModifierKeyCode};
-use ratatui::{DefaultTerminal, Frame };
-use sqlx_postgres::{PgPool, PgPoolOptions};
+use ratatui::{DefaultTerminal, Frame, layout::{Constraint, Direction, Layout, Rows}, style::Stylize, widgets::{Block, Borders, Row, Table} };
+use sqlx::{Column, Row as SqlxRow, Value, any::AnyRow};
+// use sqlx::Row;
+use sqlx_postgres::{PgColumn, PgPool, PgPoolOptions, PgRow, PgTypeInfo, PgTypeKind, Postgres};
 use tokio::io::AsyncWriteExt;
-use std::{env};
+use std::{any::Any, env};
 
 struct App<'a> {
     _conn: PgPool,
     textarea: TextArea<'a>,
     term: &'a mut DefaultTerminal,
+    results: Vec<Row<'a>>,
+}
+
+// fn as_string(mut row: PgRow, index: usize) -> String {
+//     if let value = row.get(index) {
+//         match value {
+//             Value::Null => String::from(""),
+//             Value::Bytes(v) => String::from_utf8_lossy(v.as_slice()).into_owned(),
+//             Value::Int(v) => format!("{v}"),
+//             Value::UInt(v) => format!("{v}"),
+//             Value::Float(v) => format!("{v}"),
+//             Value::Double(v) => format!("{v}"),
+//             Value::Date(year, month, day, hour, minutes, seconds, micro) => todo!(),
+//             Value::Time(negative, days, hours, minutes, seconds, micro) => todo!(),
+//         }
+//     } else {
+//         String::from("")
+//     }
+// }
+
+// enum RowE {
+//     Any(sqlx::any::AnyRow),
+//     PgRow(sqlx_postgres::PgRow),
+// }
+
+
+
+fn stringify(row: &PgRow, col: &PgColumn) -> String {
+
+        let str_type = PgTypeInfo::with_name("Text");
+        let int4_type = PgTypeInfo::with_name("Int4");
+
+        let type_info = col.type_info();
+
+        let is_str = type_info.type_eq(&str_type);
+        
+        if is_str {
+            row.get(col.name())
+        }
+
+        let is_int4 = type_info.type_eq(&int4_type);
+        
+        if is_int4 {
+            let data: i32 = row.get(col.name());
+            
+            let data_str_res = data.to_string();
+            data_str_res
+
+        } else {
+            panic!("type not implemented");
+        }
 }
 
 
@@ -23,6 +76,7 @@ impl<'a> App<'a> {
             _conn: pool,
             textarea: textarea,
             term: terminal,
+            results: Vec::new(),
         }
     }
 
@@ -31,12 +85,99 @@ impl<'a> App<'a> {
                 .fetch_all(&self._conn)
                 .await
                 .expect("failed to execute query");
-        logln(format!("{:?}", result).as_str()).await;
+            
+    
+        let mut result_strs: Vec<Row> = Vec::new();
+
+        for row in result {
+            let mut row_strs: Vec<String> = Vec::new();
+            let cols = row.columns();
+            for col in cols {
+                let str_type = PgTypeInfo::with_name("Text");
+                let int4_type = PgTypeInfo::with_name("Int4");
+
+                let type_info = col.type_info();
+
+                let is_str = type_info.type_eq(&str_type);
+                
+                if is_str {
+                    row_strs.push(row.get(col.name()));
+                }
+
+                let is_int4 = type_info.type_eq(&int4_type);
+                
+                if is_int4 {
+                    let data: i32 = row.get(col.name());
+                    
+                    let data_str_res = data.to_string();
+                    row_strs.push(data_str_res);
+                    // data_str_res
+
+                } 
+                // else {
+                //     panic!("type not implemented");
+                // }
+            }
+            result_strs.push(Row::new(row_strs));
+        }
+        
+
+        logln(format!("result_strs = {:?}", result_strs).as_str()).await;
+        self.results = result_strs;
+
+        // self.results = result;
+
+        // for ele in result {
+        //     ele
+        // }
+
+        // logln(format!("{:?}", result).as_str()).await;
     }
 
     fn draw(&mut self) {
         let _ = self.term.draw(| frame: &mut Frame |  {
-            frame.render_widget(&self.textarea, frame.area());
+    
+        let layout = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(4)
+            .constraints(vec![ 
+                Constraint::Percentage(50),
+                Constraint::Percentage(50)
+            ])
+            .split(frame.area());
+
+           // self.results.into_iter()
+    
+        // let mut row_one: Vec<String> = Vec::new();
+
+        
+    
+    
+
+        // for res in &self.results {
+        //     row_one.push(res.get("name"));
+        //     let id = res.get("id");
+        //     row_one.push(id);
+        //     // logln(format!("res = {:?}", res).as_str()).await;
+        // }
+        // self.results
+
+        let results_table = Table::default()
+            .rows(self.results.clone());
+            // .rows(Row::new(self.results));
+
+        let b = Block::default()
+            .title("results")
+            .borders(Borders::ALL);
+        
+        let editor = Block::default()
+            .title("sql editor")
+            .borders(Borders::ALL);
+        
+        frame.render_widget(&editor, layout[0]);
+        frame.render_widget(&self.textarea, editor.inner(layout[0]));
+        frame.render_widget(&b, layout[1]);
+        frame.render_widget(&results_table, b.inner(layout[1]));
         });
     }
 
@@ -53,7 +194,6 @@ impl<'a> App<'a> {
                                 match key.modifiers {
                                     KeyModifiers::CONTROL => {
                                         println!("control S");
-                                        // logln(format!("{:?}", self.textarea).as_str()).await;
                                         let content = self.textarea.lines().join("\n");
                                         logln(content.as_str()).await;
                                         self.query(content).await;
